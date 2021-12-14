c9dbd9eb5e2d1e31f399e7fff298adb66c6d4ddb
TheFamousRat <gpotato.gr@gmail.com>
2021-12-14 22:10:14 +0100

    Added function to compute lookAt transform

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import re
+import numpy as np
 
 def setSelectOfObjectAndChildren(obj, selectState):
     """
@@ -14,3 +15,24 @@ def getChildrenWithNameContaning(parent, stringToSearch):
     Returns all children of an objects whose names contain a user-provided string
     """
     return [child for child in parent.children if (re.search(stringToSearch, child.name) != None)]
+
+def lookAtFromPos(targetPos, lookPos, upVector):
+    """
+    Returns the transform to apply to an object so that it looks towards targetPos, from a given position lookPos and with a given up vector upVector
+    :param nparray targetPos: Position to which to look
+    :param nparray lookPos: Position from which to look
+    :param nparray upVector: The world up vector
+    """
+    zAxis = targetPos - lookPos
+    zAxis = zAxis / np.linalg.norm(zAxis)
+
+    yAxis = np.cross(zAxis, np.array(upVector))
+    yAxis = yAxis / np.linalg.norm(yAxis)
+
+    xAxis = np.cross(zAxis, yAxis)
+    xAxis = xAxis / np.linalg.norm(xAxis)
+
+    transform = np.concatenate([yAxis, -xAxis, -zAxis, camPos]).reshape((4, 3)).T
+    transform = np.concatenate([transform, np.array([[0.0, 0.0, 0.0, 1.0]])])
+    
+    return transform.T
